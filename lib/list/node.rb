b9c917ea4d94b6a6e92d2a8e32f8cac945366616
Indigo Casson <atamiser@gmail.com>
2010-11-12 13:51:12 -0800

    Renamed end? to at_end? because it messed with poor vim. Also attempted to make to_s, but failed.

--- lib/list/node.rb
+++ lib/list/node.rb
@@ -25,19 +25,20 @@ class Node
     
     # Is this the end of the list?
     # For reference, it is the end of the list if the next node is nil.
-    def end?
+    def at_end?
         @n.is_a? NilClass # We use this because Node overrides ==, and things get screwed up.
     end
+
     
     # This is a recursive function that goes through the list until we get to the last node, where it returns itself.
     def get_end
-        end? ? self : @n.get_end
+        at_end? ? self : @n.get_end
     end
     
     # Iterates through the items in the list, starting from this one, and going until we get to the end.
     def each &block
         block.call(@d)
-        @n.each(&block)unless end?
+        @n.each(&block)unless at_end?
     end
     
     # Used for the Comparable module.
@@ -47,7 +48,7 @@ class Node
     
     # Adds something to the end of the list. This now accepts arrays.
     def add thing
-        if end?
+        if at_end?
             if thing.is_a? Array
                 h, t = thing.ht
                 add h
@@ -63,7 +64,7 @@ class Node
     # Gets the length of the list
     def length count=0
         count += 1
-        if end?
+        if at_end?
             count
         else
             @n.length count
@@ -74,7 +75,7 @@ class Node
     def to_a
         ary = []
         ary << @d 
-        ary << @n.to_a unless end?
+        ary << @n.to_a unless at_end?
         return ary.flatten
     end
 
@@ -85,4 +86,14 @@ class Node
             @n[x-1]
         end
     end
+
+#    def to_s(x="(")
+#        if at_end?
+#            x += ')'
+#        else
+#            x += (@d.to_s + ", ")
+#            x += @n.to_s
+#        end
+#        return x
+#    end
 end
